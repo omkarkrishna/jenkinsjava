ac8e6437e49a630375dae331f5378b0f06596966
omkarkrishna <152149874+omkarkrishna@users.noreply.github.com>
2025-11-20 14:36:59 +0530

    Create Demo.java

---
+++ Demo.java
@@ -0,0 +1,7 @@
+public class Demo {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Jenkins, this is a demo build! This is in Main branch");
+        System.out.println("Current Time in Java: " + java.time.LocalDateTime.now());
+    }
+}
