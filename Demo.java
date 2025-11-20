d92266373d7e314c79ae5a43a75f309eb05519f8
omkarkrishna <152149874+omkarkrishna@users.noreply.github.com>
2025-11-20 15:09:46 +0530

    Update Demo.java

--- Demo.java
+++ Demo.java
@@ -3,6 +3,7 @@ public class Demo {
     public static void main(String[] args) {
         System.out.println("Hello Jenkins, this is a demo build! This is in Main branch");
         System.out.println("Hii");
+        System.out.println("Changed");
         System.out.println("Current Time in Java: " + java.time.LocalDateTime.now());
     }
 }
